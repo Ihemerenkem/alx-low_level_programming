24742f8554ee9d01bf4746afe97afb59b7e47b4f
ihemerenkem <kemslim.ihemere@gmail.com>
2023-09-07 18:01:57 -0700

    Hello printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - printing output with printf
+ *
+ * Return: always (0)
+ *
+ */
+int main(void)
+{
+	printf("with proper grammar, but teh outcome is a piece of art,\n");
+	return (0);
+}
