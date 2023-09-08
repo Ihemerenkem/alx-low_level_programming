6cb2378de31eef63f8c41f4a3859eb2d956d1cb9
ihemerenkem <kemslim.ihemere@gmail.com>
2023-09-07 19:54:00 -0700

    wahala

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,7 +15,7 @@ int main(void)
 
 	printf("size of a char: %lu byte(s)\n", sizeof(a));
 	printf("size of an int: %lu byte(s)\n", sizeof(b));
-	printf("size of long int: %lu byte(s)\n", sizeof(c));
+	bprintf("size of long int: %lu byte(s)\n", sizeof(c));
 	printf("size of a long long int: %lu byte(s)\n", sizeof(d));
 	printf("size of a float: %lu byte(s)\n", sizeof(e));
 	return (0);
